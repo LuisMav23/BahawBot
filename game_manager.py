4210e95132b85ef197735e78e5b8ef1cc1caf634
Mav023 <gabrielluismaverick@gmail.com>
2023-07-07 16:04:13 +0800

    updated bot

--- game_manager.py
+++ game_manager.py
@@ -1,6 +1,9 @@
 import requests
 
-# gets a random word from random-word-api
+# GAME MANAGER INTERFACE
+#   - BEGIN DEF
+#   - STOP DEF
+#   - RESTART DEF
 
 
 class GameInfoManager:
@@ -9,39 +12,49 @@ class GameInfoManager:
     
     
     def __init__(self, active_game, ctx):
-        self.active_game = active_game
+        self.active_game = active_game.upper()
         
         if self.active_game == 'WORDLE':
             self.manager = WordleGameManager(ctx)
+        else:
+            self.active_game = 'NONE'
         
     def get_active(self):
         return self.active_game
+    
+    def __del__(self):
+        self.active_game = 'NONE'
+        if self.manager:
+            del self.manager
         
 class WordleGameManager:
     ctx = None
     Word = ''
     Guesses = []
+    isGameInProgress = False
+    
     
     def __init__(self, ctx):
         self.ctx = ctx
         self.Word = self.get_wordle_word()
         self.Guesses = []
+        self.isGameInProgress = True
     
     # sends a message that the game has started
     async def begin(self):
         message = ":green_square: :yellow_square: :regional_indicator_w: :regional_indicator_o: :regional_indicator_r: :regional_indicator_d: :regional_indicator_l: :regional_indicator_e: :yellow_square: :green_square:\n"
         message += "**Welcome to wordle, here are the instructions:**\n"
         message += "- To guess type **$guess <Your Guess>**.\n- Stop the game type **$stop** (to restart type **$restart**).\n" #\n- You have 5 Tries.\n- Only guess 5 letter words (musct be a valid word).    
-        message += "- BLOCKS:\n\t- :blue_square: : Letter is not in the word\t :yellow_square: : Letter is not in the right place\t :green_square: : Letter is in the right place"
+        message += "- **BLOCKS:**\n> :blue_square:  Letter is not in the word\n> :yellow_square:  Letter is not in the right place\n> :green_square:  Letter is in the right place"
         await self.ctx.send(message)
         
+    
     async def guess(self, guess):
         if len(guess) != 5:
             await self.ctx.send('')
             return
         guessPattern = ''
         guess = guess.upper()
-        print(type(self.Word))
         # :yellow_square: :blue_square: :green_square:
         for i in range(len(self.Word)):
             if guess[i] == self.Word[i]:
@@ -58,9 +71,30 @@ class WordleGameManager:
         for guess_dict in self.Guesses:
             word_temp = guess_dict['word']
             pattern_temp = guess_dict['pattern']
-            message += f'{word_temp}\t-\t{pattern_temp}\n'
-        await self.ctx.send(message)
+            message += f'{pattern_temp}\t-\t**{word_temp}**\n'
+            
+        if guessPattern == ':green_square: :green_square: :green_square: :green_square: :green_square: ':
+            message += "**CONRATULATIONS YOU GUESSED THE WORD**"
+            self.isGameInProgress = False
         
+        if len(self.Guesses) == 6:
+            message += f"**You Lost, the word was {self.Word}**"
+            self.isGameInProgress = False
+            
+        await self.ctx.send(message)
+    
+    async def stop(self):
+        self.ctx = None
+        self.Word = ''
+        self.Guesses = []
+        self.isGameInProgress = False
+    
+    async def restart(self):
+        self.Word = self.get_wordle_word()
+        self.Guesses = []
+        self.isGameInProgress = True
+        self.ctx.send("restarting game...")
+        await self.begin()
     
     def get_wordle_word(self):
         url = "https://random-word-api.vercel.app/api?words=1&length=5&type=uppercase"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
+
 import discord
 from discord.ext import commands
+
 from game_manager import GameInfoManager
 
 
@@ -27,29 +29,46 @@ async def on_ready():
 async def begin(ctx, game):
     global game_manager
     game_manager = GameInfoManager(game, ctx)
+    if game_manager.get_active() == 'NONE':
+        await ctx.send('Invalid game')
+        del game_manager
+        game_manager = None
+        return
     await game_manager.manager.begin()
     
 
 @bot.command()
 async def guess(ctx, guess):
     global game_manager
+    if game_manager == None:
+        await ctx.send(':bangbang:**No game is currently active**:bangbang:')
+        return
+    
     if game_manager.get_active() != 'WORDLE':
-        await ctx.send('Wordle game is currently inactive')
+        await ctx.send(':bangbang:**Another game is currently active**:bangbang:')
         return
     
     await game_manager.manager.guess(guess)
-    # if guessPattern == ':green_square: :green_square: :green_square: :green_square: :green_square: ':
-    #     await ctx.send('You won!')
-    #     isGameInProgress = False
-    #     tries = 5
-    #     wordGuesses = []
-    #     wordleWord = ''
-    #     return
+    if not game_manager.manager.isGameInProgress:
+        del game_manager
+    
+@bot.command()
+async def stop(ctx):
+    global game_manager
+    if game_manager == None:
+        await ctx.send(':bangbang:**No game is currently active**:bangbang:')
+        return
+    del game_manager
+    game_manager = None
+    await ctx.send("Game Stopped")
 
 @bot.command()
-async def send(ctx):
-    await ctx.send("hello\nhello")
-
+async def restart(ctx):
+    global game_manager
+    if game_manager == None:
+        await ctx.send(':bangbang:**No game is currently active**:bangbang:')
+        return
+    await game_manager.manager.restart()
 
 if __name__ == '__main__':
-        bot.run(os.getenv('TOKEN'))
+    bot.run(os.getenv('DISCORD_BOT_TOKEN'))
